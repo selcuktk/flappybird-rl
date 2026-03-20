c37666c49f53082917b9b76b2a5a2f119bf7e485
Selçuk Talha Kul <157029095+selcuktk@users.noreply.github.com>
2026-03-20 15:50:45 +0300

    Add Hyperparameters

--- agent.py
+++ agent.py
@@ -1,42 +1,62 @@
+import itertools
 import flappy_bird_gymnasium
 import gymnasium
 from dqn import DQN
 import torch
 from experience_replay import ReplayMemory
+import yaml
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class Agent:
+    def __init__(self, hyperparameter_set):
+        with open('hyperparameters.yml', 'r') as file:
+            all_hyperparameter_sets = yaml.safe_load(file)
+            hyperparameters = all_hyperparameter_sets[hyperparameter_set]
+            # print(hyperparameters)
+
+        self.replay_memory_size = ReplayMemory(hyperparameters['replay_memory_size'])
+        self.mini_batch_size = hyperparameters['mini_batch_size']
+        self.epsilon_init    = hyperparameters['epsilon_init']
+        self.epsilon_decay = hyperparameters['epsilon_decay']
+        self.epsilon_min = hyperparameters['epsilon_min']
+
     def run(self, is_training=True, render=False):
         env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
 
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
 
+        rewards_per_episode = []
+
         policy_dqn = DQN(num_states, num_actions).to_device(device)
 
         if is_training:
-            memory = ReplayMemory(10000)
+            memory = ReplayMemory(self.replay_memory_size)
 
-
-        for episode in range(10000):
+        # basicly itertools goes infinity or smt
+        for episode in itertools.count():
             state, _ = env.reset()
-            while True:
+            terminated = False
+            episode_reward = 0.0
+            while not terminated:
                 # Next action:
                 # (feed the observation to your agent here)
                 action = env.action_space.sample()
 
                 # Processing:
                 new_state, reward, terminated, _, info = env.step(action)
+
+                # Accumulate reward
+                episode_reward += reward
+
                 if is_training:
                     memory.append((state, action, reward, terminated, info))
 
                 # Move to new state
                 state = new_state
 
-                # Checking if the player is still alive
-                if terminated:
-                    break
+            rewards_per_episode.append(episode_reward)
 
-        env.close()
+
