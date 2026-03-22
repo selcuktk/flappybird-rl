4cc7f5366dd79439dc0680f91f8ebf1c3909002b
Selçuk Talha Kul <157029095+selcuktk@users.noreply.github.com>
2026-03-22 19:15:15 +0300

    Finishing the Project

--- experience_replay.py
+++ experience_replay.py
@@ -1,7 +1,7 @@
 # Define memory for Experience Replay
 from collections import deque
 import random
-class ReplayMemory(object):
+class ReplayMemory():
     def __init__(self, maxlen, seed=None):
         self.memory = deque([], maxlen=maxlen)
 
@@ -13,7 +13,7 @@ class ReplayMemory(object):
         self.memory.append(transition)
 
     def sample(self, sample_size):
-        minibatch = random.sample(self.memory, sample_size)
+        return random.sample(self.memory, sample_size)
 
     def __len__(self):
         return len(self.memory)
